263829ab3ebdc100f3aa61039217b19f10dc9801
Damian Malysza <damian.malysza@gmail.com>
2021-10-07 18:36:01 -0400

    commit tree 85413b0ceb52cb27547d0b582d569df3f6d43a0c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+  let returnArray = []
+  for (let i = 0; i < names.length; i++) {
+    returnArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return returnArray
+}
+
+function countDown(number){
+  while (number >= 0) {
+    console.log(number)
+    number --
+  }
+}
